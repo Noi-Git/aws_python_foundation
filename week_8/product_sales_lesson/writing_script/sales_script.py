dc0a8f2df80b361a2efd63a90b8e0c585680a108
Noi-Git <noi.patsin@gmail.com>
2025-06-04 17:21:58 -0400

    add sales data to Product class

--- week_8/product_sales_lesson/writing_script/sales_script.py
+++ week_8/product_sales_lesson/writing_script/sales_script.py
@@ -5,6 +5,22 @@ from clear_terminal import clear_terminal
 clear_terminal()
 from product_sales import Product
 
+#======= Add sale data to Product ========
+product_data = [("widgetA", 5.99, 100), ("widgetB", 10.99, 100),
+            ("widgetC",14.99 , 200), ("widgetD", 16.99, 50),
+            ("widgetE",14.99 , 125), ("widgetF", 11.99, 75),
+            ("widgetG",4.99 , 200), ("widgetH", 17.99, 200),
+            ("widgetI",1.99 , 300), ("widgetJ", 13.99, 50)]
+
+def create_product_objects(data):
+    products = []
+    for product in product_data:
+        name, price, quant = product
+        products.append(Product(name, price, quant))
+    return products
+
+
+#======= Sales ========
 class Sales:
   def __init__(self):
     self.sales_data = []
